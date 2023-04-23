700f92ff490ff020e4464e3d18e1ca37efaf9dcd
korosenpai <koro.dev404@gmail.com>
2023-04-23 10:47:00 +0200

    changed to matplotlib

--- main.py
+++ main.py
@@ -1,10 +1,47 @@
+# https://matplotlib.org/stable/gallery/subplots_axes_and_figures/subplots_demo.html
+
 import serial
 from time import sleep
 from json import loads
 
-import pygame
-# from pygame.locals import *
+from matplotlib import pyplot as plt
 
+# Initialize plot
+# create matplotlib figure and axes
+fig, ax = plt.subplots(3)
+
+lines = {
+    "x": ax[0].plot([], [])[0],
+    "y": ax[1].plot([], [])[0],
+    "z": ax[2].plot([], [])[0]
+}
+
+# show the plot
+plt.show(block=False)
+
+
+# ax[0].plot()
+# ax[1].plot()
+# ax[2].plot()
+
+# Set the y-axis range
+MAX_X = 300
+for elem in ax:
+    elem.set_ylim(-20, 20)  # Set the maximum y-axis limit to 1
+    elem.set_xlim(0, MAX_X + MAX_X * 0.2)  # Set the maximum y-axis limit to 1
+    elem.get_xaxis().set_visible(False) # or -> ax.set_xticklabels([]) ## to hide x axis labels
+
+ax[0].set_title("onde p", fontstyle = "italic", loc = "left", fontsize = "medium")
+ax[1].set_title("onde s", fontstyle = "italic", loc = "left", fontsize = "medium")
+ax[2].set_title("onde superficiali", fontstyle = "italic", loc = "left", fontsize = "medium")
+
+
+x = list(range(MAX_X))
+y = {
+    "x": [], # x recieved from sensor
+    "y": [],
+    "z": []
+}
 
 # initialize
 BAUDRATE = 115200
@@ -19,105 +56,31 @@ while serial_data.in_waiting == 0:
 while (data_packet := str(serial_data.readline(), "utf-8")) != "all set up, starting to send data...\r\n":
     print(data_packet)
 
-
-
-pygame.init()
-
-AXIS_PADDING = 50
-AXIS_WIDTH = 3
-AXIS_STEP_Y = 60
-AXIS_STEP_X = AXIS_STEP_Y * 2
-AXIS_STEP__LENGTH = 10
-AXIS_STEP_VALUE = 20
-
-# screen res
-HEIGHT = 2.5 * AXIS_PADDING + AXIS_STEP_Y * 9
-WIDTH = 2.5 * AXIS_PADDING + AXIS_STEP_X * 7
-
-print(f"width: {WIDTH}, height: {HEIGHT}")
-
-# vars for graph
-ORIGINX = 1.5 * AXIS_PADDING
-ORIGINY = HEIGHT - 1.5 * AXIS_PADDING
-ORIGIN = ( ORIGINX, ORIGINY ) # position on screen of (0, 0)
-
-# elements in graph
-x = list(range(100))
-y = []
-
-
-
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("seismometer")
-
-FONT_SIZE = 20
-FONT = pygame.font.SysFont("cascadiamonoregular", FONT_SIZE)
-
+# when no data is passed
+while serial_data.in_waiting == 0:
+    pass
 
 while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
-    
-    while serial_data.in_waiting == 0:
-        pass
-
-    screen.fill((255, 255, 255))
-
-    # draw axis
-    pygame.draw.line(screen, (0, 0, 0), (ORIGINX, AXIS_PADDING), ORIGIN, AXIS_WIDTH) # y
-    pygame.draw.line(screen, (0, 0, 0), ORIGIN, (WIDTH - AXIS_PADDING, ORIGINY), AXIS_WIDTH) # x
-
-    # draw axis lines y
-    for i in range(9, 0, -1):
-        pygame.draw.line(screen, (0, 0, 0),
-            (ORIGINX + AXIS_STEP__LENGTH, ORIGINY - AXIS_STEP_Y * (i - 1)),
-            (ORIGINX - AXIS_STEP__LENGTH, ORIGINY - AXIS_STEP_Y * (i - 1)),
-        AXIS_WIDTH)
-
-        y_graph_number = FONT.render(str(AXIS_STEP_VALUE - 5 * (i - 1)) , False, (0, 0, 0))
-        screen.blit(y_graph_number,
-            (
-                ORIGINX - 2 * AXIS_STEP__LENGTH - y_graph_number.get_width(),
-                AXIS_PADDING + AXIS_STEP_Y * i - y_graph_number.get_height() / 2
-            )
-        )
-    
-    # draw axis lines x
-    for i in range(7):
-        pygame.draw.line(screen, (0, 0, 0),
-            (ORIGINX + AXIS_STEP_X * i, ORIGINY - AXIS_STEP__LENGTH),
-            (ORIGINX + AXIS_STEP_X * i, ORIGINY + AXIS_STEP__LENGTH),
-        AXIS_WIDTH)
-
-        x_graph_number = FONT.render(str(AXIS_STEP_VALUE * i) , False, (0, 0, 0))
-        screen.blit(x_graph_number,
-            (
-                ORIGINX + AXIS_STEP_X * i - x_graph_number.get_width() / 2,
-                ORIGINY + 2 * AXIS_STEP__LENGTH - x_graph_number.get_height() / 2
-            )
-        )
-
-
-    ########## add accel data to graph ##########
 
     # read data
     data_packet = serial_data.readline()
     data_packet = loads(str(data_packet, "utf-8"))
 
-
     print(data_packet)
 
-    if len(y) >= 100:
-        y.pop(0)
+    for n, axis in enumerate(y):
+        if len(y[axis]) >= MAX_X:
+            y[axis].pop(0)
 
-    y.append(data_packet["accel"][0])
+        y[axis].append(data_packet["accel"][n])    
 
-    # TODO
+        lines[axis].set_xdata(x[MAX_X - len(y[axis]):]) # print only x to same number of 'y[axis]' values # starting from end so then it gets traslated
+        lines[axis].set_ydata(y[axis])
 
-    # pygame.draw.lines(screen, (0, 0, 0), False, [ORIGINX + ], 5)
+    ax[0].relim()
+    ax[1].relim()
+    ax[2].relim()
+    # ax.autoscale_view()
+    fig.canvas.draw()
+    fig.canvas.flush_events()
 
-
-
-    pygame.display.flip() # TODO or update or blit? check which one to use
